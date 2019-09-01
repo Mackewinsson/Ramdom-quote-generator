a4b4872c27b7b84242cf4babc8c3a9476b67c329
Mackewinsson P <mackewinsson@gmail.com>
2019-09-01 12:22:18 -0400

    Agrego funcionalidades
    Le agrego una array de colores y la cambio aleatoriamente

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ var quotes = [
   {
     quote:'The road to success and the road to failure are almost exactly the same.',
     source:'Colin R. Davis',
+    tags:'Success',
   },
   {
     quote:'Your time is limited, so don\'t waste it living someone else\'s life. Don\'t be trapped by dogma – which is living with the results of other people\'s thinking.',
@@ -30,7 +31,18 @@ var quotes = [
     source:'Steve Jobs',
     citation:'Standford University',
     year:'2005',
+    tags:'Motivation',
   },
+];
+
+// Aqui creamos una array con objetos de propiedad color
+
+var colors = [
+  {color:'#011627'},
+  {color:'#E71D36'},
+  {color:'#E71D36'},
+  {color:'#FF9F1C'},
+  {color:'#247BA0'},
 ];
 
 /***
@@ -42,6 +54,14 @@ function getRandomQuote (){
   var ramdomNumber = Math.floor( Math.random() * 5 );
   return quotes[ramdomNumber];
 };
+
+function getRandomColor (){
+  // Aqui genero un numero aleatorio entre 0 y 5
+    var ramdomColor = Math.floor( Math.random() * 5 );
+    return colors[ramdomColor];
+  };
+
+var myTimer = setInterval(printQuote, 2000); //Aqui colocamos un contador para ejecutar el print
 
 /***
   Aqui se crea la funcion print que crea el string para ver la frase aleatoria
@@ -56,17 +76,21 @@ function printQuote(){
     html += '<span class="citation"></span>' + quoteToPrint.citation + '</span>';
   };
   if (quoteToPrint.year){
-    html += '<span class="year"></span>' + quoteToPrint.year + '</span>';
+    html += '<span class="year">' + quoteToPrint.year + '</span>';
+  };
+  if (quoteToPrint.tags){
+    html += '<br><br><span class="tags"> ' + quoteToPrint.tags + '</span>';
   };
   html += '</p>';
   var div = document.getElementById('quote-box')
   div.innerHTML = html;
+  var colorToPrint = getRandomColor();
+  var color = colorToPrint.color;
+  document.body.style.background = color;
 };
 
 /***
-  La funcion print hace que se imprima una frase aleatoria 
-  en el div quote-box y luego con el event listener cuando se presiona el boton
-  Cambia la frase.
+  Aqui se crea la funcion print para cambiar el background
 ***/
 
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
